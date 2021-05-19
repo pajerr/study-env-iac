2c9129f605d53e24c9ff81082fb39309874de2bd
Erno <54817547+pajerr@users.noreply.github.com>
2021-05-19 21:17:33 +0300

    Enabled s3 remote state backend

--- terraform/global/s3/main.tf
+++ terraform/global/s3/main.tf
@@ -2,14 +2,13 @@ provider "aws" {
     region = "eu-central-1"
 }
 
-#Uncommented only after below s3 resource has been deployed first
-#terraform { 
-#    backend "s3" { 
-#      key             = "studyenv/global/s3/terraform.tfstate"
-#      region          = "eu-central-1"
-#      encrypt         = true
-#    }
-#}
+#Uncommented backedn block only after below s3 resource has been deployed first
+#bucket name and other variables need to be defined in backend.hcl
+terraform { 
+    backend "s3" { 
+      key = "studyenv/global/s3/terraform.tfstate"
+    }
+}
 
 #Chicken and egg problem, first have to deploy AWS S3 backend and then uncomment 
 #terraform backend s3 block to use s3 as remote backend
@@ -18,7 +17,7 @@ resource "aws_s3_bucket" "terraform_state" {
     bucket          = var.bucket_name
 
     lifecycle { 
-        prevent_destroy = true 
+        prevent_destroy = false 
     }
 
   # Enable versioning so we can see the full revision history of our
@@ -37,13 +36,14 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
+#not using dynamodb as nobody else is working on code
 #resource "aws_dynamodb_table" "terraform_locks" {
 #  name         = var.dynamodb_table
 #  billing_mode = "PAY_PER_REQUEST"
 #  hash_key     = "LockID"
 
-#  attribute {
-#    name = "LockID"
-#    type = "S"
+  #attribute {
+   # name = "LockID"
+    #type = "S"
 #  }
 #}
