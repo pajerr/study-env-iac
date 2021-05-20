43d565589e436e82a2c3de7819c6b8b7de127410
Erno <54817547+pajerr@users.noreply.github.com>
2021-05-20 21:14:08 +0300

    updated ubuntu AMI to local variables

--- terraform/global/mgmt/services/ci-server/main.tf
+++ terraform/global/mgmt/services/ci-server/main.tf
@@ -13,11 +13,11 @@ locals {
   any_port = 0 
   any_protocol = "-1" 
   tcp_protocol = "tcp" 
-  all_ips = ["0.0.0.0/0"] 
+  all_ips = ["0.0.0.0/0"]
+  ubuntu_20_04_ami = "ami-0980c5102b5ef10cc" 
 }
 
 ##########################################################################
-
 #Data source to look up default VPC in AWS
 data "aws_vpc" "default" {
   default = true
@@ -36,7 +36,7 @@ resource "aws_security_group" "instance" {
   name = "${var.environment_name}-instance"
 }
 
-#Security group rule
+#Allow SSH to EC2 instance
 resource "aws_security_group_rule" "instance_allow_ssh" {
   type                = "ingress"
   security_group_id   = aws_security_group.instance.id
@@ -47,7 +47,7 @@ resource "aws_security_group_rule" "instance_allow_ssh" {
   cidr_blocks         = ["${var.myip}/${var.cidr}"]
 }
 
-# Allow all outbound traffic
+#Allow all outbound traffic
 resource "aws_security_group_rule" "instance_allow_all_outbound" {
   type                = "egress" 
   security_group_id   = aws_security_group.instance.id
@@ -62,10 +62,9 @@ resource "aws_security_group_rule" "instance_allow_all_outbound" {
 ### EC2 instance resource configurations 
 
 resource "aws_instance" "ci-server" {
-    ami                     = "ami-0980c5102b5ef10cc"
+    ami                     = local.ubuntu_20_04_ami
     instance_type           = "t2.micro"
     vpc_security_group_ids  = [aws_security_group.instance.id]
-    #security_groups         = [aws_security_group.instance.id]
 
   # the public SSH key
     key_name                = aws_key_pair.ciserver.key_name
